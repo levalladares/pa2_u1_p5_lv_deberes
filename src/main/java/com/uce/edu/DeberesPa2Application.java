69fb425b4cdaf4bf106e8672025356b48fea9700
Luis <levalladares@uce.edu.ec>
2023-11-20 21:28:34 -0500

    Tarea3

---
+++ src/main/java/com/uce/edu/DeberesPa2Application.java
@@ -0,0 +1,21 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeberesPa2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeberesPa2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("<<<<<NUEVO PROYECTO>>>>>");
+		
+	}
+
+}
---
+++ src/test/java/com/uce/edu/DeberesPa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeberesPa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
